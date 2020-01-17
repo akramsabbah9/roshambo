63bcc775a5142f2348b088c10cb9b28fc18b6be3
JakinHuangWang <darkfact247@gmail.com>
2020-01-16 22:59:04 -0800

    Found some Errorsgit add . Revert back.

--- frontend/js/components/HelloWorld/index.js
+++ frontend/js/components/HelloWorld/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Form} from 'react-bootstrap';
 
 const HelloWorld = () => {
   return (
     <>
     <p>Hello, world!</p>
-    <Button variant = "success">Success</Button>
-    <Button variant = "primary">Error</Button>
     </>
   );
 }
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById('reactHook'));
