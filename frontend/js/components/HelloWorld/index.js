84447f133d1cc44fb3b08a22f782104062163a9b
JakinHuangWang <darkfact247@gmail.com>
2020-01-16 22:24:13 -0800

    Added react-bootstrap

--- frontend/js/components/HelloWorld/index.js
+++ frontend/js/components/HelloWorld/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Form} from 'react-bootstrap';
 
 const HelloWorld = () => {
   return (
+    <>
     <p>Hello, world!</p>
+    <Button variant = "success">Success</Button>
+    <Button variant = "primary">Error</Button>
+    </>
   );
 }
 
