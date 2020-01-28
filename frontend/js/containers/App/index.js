1edb0b187bbfe40293f50d41009bfec4b4521b7a
JakinHuangWang <darkfact247@gmail.com>
2020-01-28 10:29:19 -0800

    Deleted Unneccessary files

--- frontend/js/containers/App/index.js
+++ frontend/js/containers/App/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {browserHistory, BrowserRouter, Route, Switch, Router} from 'react-router-dom';
-import Another from '../../components/HelloWorld/Another';
+import {BrowserRouter, Route, Switch, Router} from 'react-router-dom';
 import LoginPage from '../../components/LoginPage/index';
 import Register from '../../components/LoginPage/Register';
 
 
 const App = () => (
-  <BrowserRouter history={browserHistory}>
+  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={LoginPage}/>
       <Route exact path="/Register" component={Register}/>
-      <Route exact path="/HelloWorld/Another" component={Another}/>
     </Switch>
   </BrowserRouter>
 );
