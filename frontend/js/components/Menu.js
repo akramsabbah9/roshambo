03c71584b52a33e1cf65df9489b1c0447ecbe4a6
JakinHuangWang <darkfact247@gmail.com>
2020-02-04 10:15:07 -0800

    Added frist version of Menu

--- frontend/js/components/Menu.js
+++ frontend/js/components/Menu.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Form, Container,
-Col,
-Row} from 'react-bootstrap';
+import {Button, Form, Container, Col, Row} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPaper, faHandScissors, faHandRock} from '@fortawesome/free-solid-svg-icons'
 import './Menu.css'
 
 class Menu extends Component{
@@ -12,7 +12,11 @@ class Menu extends Component{
     }
     render(){
         return(
+            <div>
             <Container className="Menu">
+              <Row>
+                <FontAwesomeIcon icon={faHandPaper}/>
+              </Row>
               <Row>
                 <p className="Logo">Roshambo</p>
               </Row>
@@ -21,8 +25,14 @@ class Menu extends Component{
                   <Link to="/Register" className="mr-5">Register</Link>
                   <Link to="/Login">Login</Link>
                 </div>
+                <FontAwesomeIcon icon={faHandScissors}/>
+              </Row>
+              <Row> 
+               <FontAwesomeIcon icon={faHandRock}/>
               </Row>
             </Container>
+                
+            </div>
         )
     }
 }
