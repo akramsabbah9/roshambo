e4a9db40fd67f2abdc591d21b6f8acb34e46f658
David Macaraeg <djmmacaraeg@gmail.com>
2020-02-11 11:27:25 -0800

    Creates Stripe functionality within isolated program. Begins integrating Stripe with roshambo

--- paymentPart/src/djangostripe/urls.py
+++ paymentPart/src/djangostripe/urls.py
@@ -1,5 +1,4 @@
-"""
-roshambo URL Configuration
+"""djangostripe URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,22 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
-from rest_framework import routers
-from profile_manager import views
 from django.contrib import admin
+from django.urls import path, include
 
-from chat.views import index
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('')
-    path('chat/', include('chat.urls')),
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('admin/', admin.site.urls),
+    path('payments/', include('payments.urls'))
 ]
