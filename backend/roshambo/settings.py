8f5eb105837bf5e46a9f71b9edcdeabc0b01cb74
David Macaraeg <djmmacaraeg@gmail.com>
2020-02-10 22:13:23 -0800

    fixes redis error of --> Multiple exceptions: [Errno 111] Connect call failed ('127.0.0.1', 6379), [Errno 99] Cannot assign requested address

--- backend/roshambo/settings.py
+++ backend/roshambo/settings.py
@@ -143,7 +143,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('localhost', 6379)],
+            "hosts": [('roshambo_redis', 6379)],
         },
     },
 }
