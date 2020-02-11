50ccff867f1d16e05e0231f68cf00dc77fa66f5e
Akram Sabbah <akramsabbah9@gmail.com>
2020-02-11 13:25:00 -0800

    duplicate Room names can no longer be created. Rooms are now locked when 2 people have entered, stopping others from entering even if someone leaves the room.

--- backend/matchup/models.py
+++ backend/matchup/models.py
@@ -8,6 +8,9 @@ class Room(models.Model):
     # Number of people in room
     usr_count = models.IntegerField(default=0)
 
+    # Has the room been filled, locking it from further entry even if usr_count drops?
+    lock = models.BooleanField(default=False)
+
     def __str__(self):
         return self.title
 
--- backend/matchup/utils.py
+++ backend/matchup/utils.py
@@ -7,18 +7,27 @@ import string
 @database_sync_to_async
 def get_room_name():
     # find a room
-    rooms = list(Room.objects.filter(usr_count__lte=1))
+    rooms = list(Room.objects.filter(usr_count__lte=1, lock=False))
     if len(rooms) == 0:
         # if no rooms are open, make a new one
         name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
-        #while Room.objects.get(title=name):
-        #    name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+        # if a duplicate name is made, redo the name until it isn't a dupe: 
+        collision = 1
+        while collision == 1:
+            try:
+                Room.objects.get(title=name)
+                name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+            except Room.DoesNotExist:
+                collision = 0
+        
         room = Room.objects.create(title=name)
     else:
         room = rooms[0]
         name = rooms[0].title
     # increment usr_count of that room, and get out
     room.usr_count += 1
+    if room.usr_count == 2:
+        room.lock = True
     room.save()
     return name
 
