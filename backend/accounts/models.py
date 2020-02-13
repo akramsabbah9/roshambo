9838ac9a560f3ed8b0b1e3f26378ae77ac763df5
Benji Brandt <benjibrandt@ucla.edu>
2020-02-13 12:28:00 -0800

    [accounts] adds model changes and migrations for UserStats, AvailableSkins and PurchasedSkins.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -11,6 +11,7 @@ class RoshamboUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True) # changes email to unique and blank to false
     country_code = models.PositiveSmallIntegerField(default=0)
     guild = models.CharField(max_length=30, default='')
+    active_skin = models.PositiveIntegerField(default=0)
 
     # remove unnecessary fields from AbstractUser
     is_superuser = None
@@ -19,3 +20,22 @@ class RoshamboUser(AbstractUser):
 
     def __str__(self):
         return 'email: {}; username: {}'.format(self.email, self.username)
+
+
+class UserStats(models.Model):
+    user = models.ForeignKey(RoshamboUser, on_delete=models.CASCADE, primary_key=True)
+    games_played = models.PositiveIntegerField(default=0)
+    games_won = models.PositiveIntegerField(default=0)
+    games_lost = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return ''
+
+
+class AvailableSkins(models.Model):
+    skin = models.PositiveIntegerField(primary_key=True)
+
+
+class PurchasedSkins(models.Model):
+    user = models.ForeignKey(RoshamboUser, on_delete=models.CASCADE, primary_key=True)
+    skin = models.ForeignKey(AvailableSkins, on_delete=models.CASCADE)
