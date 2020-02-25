f4f558df138efe4fcd86ddea883ff5b7ce429fa4
Benji Brandt <benjibrandt@ucla.edu>
2020-02-24 21:15:27 -0800

    Tidies up main folder of repo (removes unnecessary files).

--- paymentPart/src/payments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('charge/', views.charge, name='charge'),
-    path('payments/', views.HomePageView.as_view(), name='home'),
-]
