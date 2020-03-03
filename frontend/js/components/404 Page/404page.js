46486b911f9c2fa9623752f9c00398967d691144
johnson-chou8 <johnson.chou6888@gmail.com>
2020-03-03 10:55:18 -0800

    Fixed styling of 404 page

--- frontend/js/components/404 Page/404page.js
+++ frontend/js/components/404 Page/404page.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 
 
 class Erehwon extends Component {
     render(){
         return(
-            <h1>404 Page Not Found</h1>
+            <Container>
+                <div style={{marginTop: '25%'}}/>
+                <Col className="col d-flex align-items-center justify-content-center">
+                    <Row>
+                        <FontAwesomeIcon icon={faMinusCircle} style={{color: 'red'}} size='6x' />
+                        <h1 style={{marginLeft: 40, marginTop: 17}}>404 Page Not Found</h1>
+                    </Row>
+                </Col>
+            </Container>
         )
     }
 }
